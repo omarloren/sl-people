b389a21fbbb0fac0c4116ff21f1ad6acf92d627c
Omar L <olopez@itexico.net>
2020-08-28 14:57:16 -0500

    Add list of people records

--- src/js/containers/people.jsx
+++ src/js/containers/people.jsx
@@ -3,21 +3,52 @@ import css from 'css/Page';
 import moment from 'moment'
 import { connect, useDispatch } from 'react-redux';
 
-import { Grid, Header, Icon, Search, Segment, Image, Dimmer, Loader } from 'semantic-ui-react'
+import { Table, Button } from 'semantic-ui-react'
 import { getAll } from 'actions/people'
 
 function People(props) {
     const { data } = props;
     const dispatch = useDispatch();
-
+    
     useEffect(() => {
         if (data === null ) {
             dispatch(getAll())
         }
     }, [data])
 
+    if (data === null) {
+        return null;
+    }
+
     return (
-        <h1>HOLA</h1>
+        <div class={css.container}>
+            <h3>People</h3>
+            <div class={css.actions}>
+                <Button size='tiny' primary>Show Frequency</Button>
+                <Button size='tiny' primary>Show Duplicates</Button>
+            </div>
+
+            <Table celled striped>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Name</Table.HeaderCell>
+                        <Table.HeaderCell>Email</Table.HeaderCell>
+                        <Table.HeaderCell>Job Title</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                {
+                    data.map((person) => (
+                        <Table.Row>
+                            <Table.Cell>{person.display_name}</Table.Cell>
+                            <Table.Cell>{person.email_address}</Table.Cell>
+                            <Table.Cell>{person.title}</Table.Cell>
+                        </Table.Row> 
+                    ))
+                }
+                </Table.Body>
+            </Table>
+        </div>
     )
 }
 
--- src/js/reducer/people.jsx
+++ src/js/reducer/people.jsx
@@ -2,7 +2,8 @@
 import update from 'immutability-helper';
 
 import {
-    
+    GET_ALL_RECEIVE,
+    GET_ALL_REQUEST
 } from 'actions/people';
 
 const InitialState = {
@@ -11,6 +12,17 @@ const InitialState = {
 };
 
 export default function(state = InitialState, action) {
-    console.log(action)
+    switch (action.type) {
+        case GET_ALL_REQUEST:
+            return update(state, {
+                loading: { $set: true }
+            })
+        case GET_ALL_RECEIVE:
+            return update(state, {
+                loading: { $set: true },
+                data: { $set: action.payload.data }
+            })
+    }
+    console.log(action) 
     return state
 }
