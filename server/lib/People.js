27d00852f1efde10bf3661b369d2064ec9b4e12e
Omar L <olopez@itexico.net>
2020-08-29 13:12:06 -0500

    add frequency and emaul duplicates functionalities

--- server/lib/People.js
+++ server/lib/People.js
@@ -33,7 +33,7 @@ class People {
         this._data = data;
     }
 
-    async frequency() {
+    frequency() {
         const frequency = {}
         
         this._data.map((person) => {
@@ -54,6 +54,67 @@ class People {
         return frequency;
     }
 
+    oneEditReplace(str1, str2) {
+        let differenceFound = false;
+        for (var i in str1) {
+            if (str1[i] !== str2[i]) {
+                if (differenceFound) {
+                    return false
+                }
+                differenceFound = true;
+            }
+        }
+        return true;
+    }
+
+    oneEditInsert(str1, str2) {
+        let i1 = 0;
+        let i2 = 0;
+
+        while (i2 < str2.length && i1 < str1.length) {
+            if (str1[i1] !== str2[i2]) {
+                if (i1 !== i2) {
+                    return false;
+                }
+                i2++;
+            } else {
+                i1++;
+                i2++;
+            }
+        }
+        return true;
+    }
+
+    oneEditAway(str1, str2) {
+        if (str1.length === str2.length) {
+            return this.oneEditInsert(str1, str2);
+        } else if (str1.length + 1 === str2.length) {
+            return this.oneEditInsert(str1, str2);
+        } else if (str1.length - 1 === str2.length) {
+            return this.oneEditInsert(str1, str2);
+        }
+        return false;
+    }
+
+    /**
+     * Finds possible duplicate email addresses in the data set, based on whether or not a given is 
+     * one edit/insert away from being equal 
+     */
+    emailDuplicates() {
+        let addresses = [];
+        let duplicates = []
+        addresses = this._data.map((person) => person.email_address);
+
+        for (let i = 0; i < addresses.length; i++) {
+            const address = addresses[i];
+            for (let j = i + 1; j < addresses.length; j++) {
+                if (this.oneEditAway(address, addresses[j])) {
+                    duplicates.push([address, addresses[j]])
+                }
+            }
+        }
+        return duplicates;
+    }
 }
 
 module.exports.People = People
--- server/tests/people.test.js
+++ server/tests/people.test.js
@@ -9,7 +9,9 @@ const dataMock = [
     { 'email_address': 'last@me.com' },
     { 'email_address': 'newemailisnew@menew.com' },
     { 'email_address': 'testt@gmail.com' },
-    { 'email_address': 'raa_beetty@google.com' }
+    { 'email_address': 'raa_beetty@google.com' },
+    { 'email_address': 'benoliv@salesloft.com' },
+    { 'email_address': 'benolive@salesloft.com' }
 ]
 
 describe("People Test", () => {
@@ -22,6 +24,19 @@ describe("People Test", () => {
     it('Calculates the frequency count of all unique characters in all email addresses from a given set of data', () => {
         const people = new People();
         people.data = dataMock;
-        const frequency = people.frequency()
+        const {s, a, k, '@': at } = people.frequency();
+        expect(s).to.equal(12);
+        expect(a).to.equal(13);
+        expect(k).to.equal(2);
+        expect(at).to.equal(8);
+    })
+
+    it('find possible duplicates in people data by email_address', () => {
+        const people = new People();
+        people.data = dataMock;
+
+        const duplicates = people.emailDuplicates()
+        expect(duplicates[0]).to.eql([ 'sakatius@gmail.com', 'sakatiuss@gmail.com' ]);
+        expect(duplicates[1]).to.eql([ 'benoliv@salesloft.com', 'benolive@salesloft.com' ]);
     })
 })
